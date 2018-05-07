11552ec3589b4568b0244c94a747edb331f7f69f
Ajain <ajainvivek07@gmail.com>
2018-05-07 14:10:41 +1000

    Add mint-ui

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <mt-header fixed title="Star Wars"></mt-header>
     <message-toggle />
     <list :items="['list item 1', 'listen item 2']" />
   </div>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import 'mint-ui/lib/style.css'
 import App from './App.vue'
+
+import Mint from 'mint-ui';
+Vue.use(Mint);
 
 new Vue({
   el: '#app',
